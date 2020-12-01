14c83aaae0e51f9ef3111a3e0e0334438ca5e8a9
Aditya anand <adityaanandofficial@gmail.com>
2020-12-02 01:25:08 +0530

    first commit

---
+++ middleware/authAdmin.js
@@ -0,0 +1,14 @@
+const USers = require("../models/userModel")
+const authAdmin = async(req, res, next) => {
+    try {
+        const user = await USers.findOne({
+            _id: req.user.id
+        })
+        if (user.role == 0)
+            return res.status(400).json({ msg: "Admin resources access denied" })
+        next()
+    } catch (err) {
+        return res.status(500).json({ msg: err.message })
+    }
+}
+module.exports = authAdmin
